696d0ce13409f92477d49a95be07b9e5d818bf3e
Catarina Isabel Afonso Pires Marques <caafonso@c2r7s13.42lisboa.com>
2024-10-30 17:40:59 +0000

    test push

---
+++ Libft/ft_strlen.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlen.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: caafonso <caafonso@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/30 12:21:08 by caafonso          #+#    #+#             */
+/*   Updated: 2024/10/30 12:52:16 by caafonso         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_strlen(int c)
+{
+    return (c > 32 && c < 126);
+}
+
+/*#include <stdio.h>
+int	main(void)
+{
+    printf("c %d\n", ft_isprint('c'));
+    printf("2 %d\n", ft_isprint('2'));
+    printf("  %d\n", ft_isprint(' '));
+    printf(", %d\n", ft_isprint(','));
+    printf("V %d\n", ft_isprint('V'));
+}
+*/
