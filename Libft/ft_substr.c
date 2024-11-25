851469ac0ecfea38098f4e6d1a0fadbfcb57d685
Catarina Isabel Afonso Pires Marques <caafonso@c2r8s12.42lisboa.com>
2024-11-25 20:00:49 +0000

    1st try

--- Libft/ft_substr.c
+++ Libft/ft_substr.c
@@ -6,7 +6,28 @@
 /*   By: caafonso <caafonso@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/23 19:34:23 by caafonso          #+#    #+#             */
-/*   Updated: 2024/11/23 19:34:24 by caafonso         ###   ########.fr       */
+/*   Updated: 2024/11/25 19:57:26 by caafonso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
+
+char *ft_substr(const char *s, unsigned int start, size_t len)
+{
+	size_t	i;
+	char	*result;
+
+	if (!s)
+		return (NULL);
+	result = malloc(len + 1);
+	if (!result)
+		return (NULL);
+	i = 0;
+	while (i < len)
+	{
+		result[i] = s[i + start];
+		i++;
+	}
+	result[i] = '\0';
+	return (result);
+}
