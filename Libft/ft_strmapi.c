40ef473bfc79af6dbb1e405c361cd6aef8ce549a
Catarina Isabel Afonso Pires Marques <caafonso@c2r8s12.42lisboa.com>
2024-11-26 17:09:03 +0000

    trim falta main, itoa/split/mapi/iteri vazios

--- Libft/ft_strmapi.c
+++ Libft/ft_strmapi.c
@@ -1,35 +1,14 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strtrim.c                                       :+:      :+:    :+:   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: caafonso <caafonso@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/18 20:16:33 by caafonso          #+#    #+#             */
-/*   Updated: 2024/11/26 17:06:50 by caafonso         ###   ########.fr       */
+/*   Created: 2024/11/26 15:05:26 by caafonso          #+#    #+#             */
+/*   Updated: 2024/11/26 15:06:01 by caafonso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-//elimina inicio e fim q tenham letras do set
-char	*ft_strtrim(char const *s1, char const *set)
-{
-	size_t	start;
-	size_t	end;
-
-	if (ft_strlen(s1) == 0)
-		return (ft_strdup(""));
-	start = 0;
-	end = ft_strlen(s1);
-	while (s1[start] != NULL && ft_strchr(set, s1[start]))
-		start++;
-	while (end > start && ft_strchr(set, s1[end]))
-		end--;
-	return (ft_substr(s1, start, end - start));
-}
-
-int	main(void)
-{
-	
-}
