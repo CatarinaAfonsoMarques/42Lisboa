846c0209a41313a4367a8d519ca185d47bd27e3d
Catarina Isabel Afonso Pires Marques <caafonso@c1r3s12.42lisboa.com>
2024-12-20 20:17:20 +0000

    files

--- Printf/ft_putstr.c
+++ Printf/ft_putstr.c
@@ -1,39 +1,14 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*   ft_putstr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: caafonso <caafonso@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/12/09 16:35:59 by marvin            #+#    #+#             */
-/*   Updated: 2024/12/20 20:15:08 by caafonso         ###   ########.fr       */
+/*   Created: 2024/12/20 19:45:04 by caafonso          #+#    #+#             */
+/*   Updated: 2024/12/20 19:52:06 by caafonso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-//malloc, free, write, va_start, va_arg, va_copy, va_end
-/*
-access variable arguments with list
-if % then check format else increment
-*/
-
 #include "libftprintf.h"
 
-int	ft_printf(const char *format, ...)
-{
-	va_list list;
-
-	va_start(list, format);
-	while (*format)
-	{
-		if (format == %)
-		{
-			ft_format();
-		}
-		else
-		{
-			
-		}
-	}
-	va_end(list);
-	return (0);
-}
--- Printf/libftprintf.h
+++ Printf/libftprintf.h
@@ -1,39 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*   libftprintf.h                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: caafonso <caafonso@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/12/09 16:35:59 by marvin            #+#    #+#             */
-/*   Updated: 2024/12/20 20:15:08 by caafonso         ###   ########.fr       */
+/*   Created: 2024/12/20 19:48:33 by caafonso          #+#    #+#             */
+/*   Updated: 2024/12/20 19:51:55 by caafonso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-//malloc, free, write, va_start, va_arg, va_copy, va_end
-/*
-access variable arguments with list
-if % then check format else increment
-*/
+#ifndef LIBFTPRINTF_H
+# define LIBFTPRINTF_H
 
-#include "libftprintf.h"
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <limits.h>
+# include <strings.h>
+# include <stdarg.h>
 
-int	ft_printf(const char *format, ...)
-{
-	va_list list;
+//PROTOTIPAGENS
 
-	va_start(list, format);
-	while (*format)
-	{
-		if (format == %)
-		{
-			ft_format();
-		}
-		else
-		{
-			
-		}
-	}
-	va_end(list);
-	return (0);
-}
+#endif
