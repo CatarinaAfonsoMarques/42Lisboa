5f3aa84bd2d025acca7c77cfea77566bf61afe29
Catarina Isabel Afonso Pires Marques <caafonso@c2r6s10.42lisboa.com>
2025-01-31 22:01:46 +0000

    basicamente nada :)

--- GetNextLine/get_next_line.c
+++ GetNextLine/get_next_line.c
@@ -6,17 +6,35 @@
 /*   By: caafonso <caafonso@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/29 15:46:36 by caafonso          #+#    #+#             */
-/*   Updated: 2025/01/29 15:49:02 by caafonso         ###   ########.fr       */
+/*   Updated: 2025/01/31 20:18:15 by caafonso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-/* learn variadic functions
+#include "get_next_line.h"
 
+/* learn variadic functions
+compile with D BUFFER_SIZE=n
 */
 #include "get_next_line.h"
 
 char *get_next_line(int fd)
 {
+	char *buff;
+
+	
 	if (!fd)
 		return ("NULL");
+	buff = malloc( + 1);
+	buff = read();
+	if (success)
+	{
+		while (len - 1)
+		{
+			buff[i] = '\n'; // nao na ultima
+		}
+
+		return (buff);
+	}
+	free(buff);
+	return (NULL);
 }
---
+++ GetNextLine/get_next_line.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: caafonso <caafonso@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/31 20:08:23 by caafonso          #+#    #+#             */
+/*   Updated: 2025/01/31 20:08:45 by caafonso         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+//PROTOTIPAGENS
+
+
+#endif
