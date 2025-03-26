f419caecf2a359de63fc2e21764ae6346d6ff01f
CatarinaAfonsoMarques <catarinaafonsomarques@gmail.com>
2025-03-26 17:14:48 +0000

    cenas push

---
+++ PushSwap/checker.c
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   checker.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: caafonso <caafonso@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/25 15:17:37 by caafonso          #+#    #+#             */
+/*   Updated: 2025/03/25 17:33:34 by caafonso         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+calculates and displays the shortest sequence of 
+Push_swap instructions needed to sort the given integers.
+
+2 stacks named a and b
+Beggining: The stack a contains a random number of unique negative and/or positive
+integers. The stack b is empty.
+
+read, write, malloc, free, exit, ft_printf & libft
+
+sa (swap a): Swap the first 2 elements at the top of stack a.
+Do nothing if there is only one element or none.
+sb (swap b): Swap the first 2 elements at the top of stack b.
+Do nothing if there is only one element or none.
+ss : sa and sb at the same time.
+
+pa (push a): Take the first element at the top of b and put it at the top of a.
+Do nothing if b is empty.
+pb (push b): Take the first element at the top of a and put it at the top of b.
+Do nothing if a is empty.
+
+ra (rotate a): Shift up all elements of stack a by 1.
+The first element becomes the last one.
+rb (rotate b): Shift up all elements of stack b by 1.
+The first element becomes the last one.
+rr : ra and rb at the same time.
+
+rra (reverse rotate a): Shift down all elements of stack a by 1.
+The last element becomes the first one.
+rrb (reverse rotate b): Shift down all elements of stack b by 1.
+The last element becomes the first one.
+rrr : rra and rrb at the same time.
+
+
+*/
---
+++ PushSwap/push_swap.c
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: caafonso <caafonso@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/25 15:17:14 by caafonso          #+#    #+#             */
+/*   Updated: 2025/03/25 17:34:23 by caafonso         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_swap(char c)
+{
+	// recieves a stack and swaps its first 2 
+	// nothing if len <= 1
+}
+
+void	ft_push()
+{
+	// top of a goes to top of b
+	// nothing if []
+}
+
+void	ft_rotate(char c)
+{
+	// Shift up all elements of stack a by 1.
+	// The first element becomes the last one.
+}
+
+void	ft_rev_rotate()
+{
+	//Shift down all elements of stack a by 1.
+	//The last element becomes the first one.
+}
+
+void	push_swap(char *stack_a)
+{
+	//If no parameters, not display anything and return to the prompt.
+	//In case of error, it must display "Error" followed by an ’\n’ on the standard error.
+	//Errors include, for example: some arguments not being integers, some arguments
+	//exceeding the integer limits, and/or the presence of duplicates.
+	
+	//create stack b
+	//organize
+	
+	//return (shortest sequence of instructions needed to sort stack a, separated by \n)
+}
+int	main(void)
+{
+	
+}
